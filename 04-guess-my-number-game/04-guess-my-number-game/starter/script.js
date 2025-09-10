7f6a5bef0e13021d6ffacd9df1460eee499f47b4
CodedCanvasByGab <lheegabriel.hosena@gmail.com>
2025-09-10 12:46:13 +0800

    feat: implement game foundation and DOM basics
    - Master DOM element selection with querySelector and querySelectorAll
    - Create game state management with variables and data persistence
    - Implement basic game logic with event listeners and conditional statements
    - Build interactive "Guess My Number" game foundation with core mechanics
    - Establish professional game development patterns and user interaction
    with '#' will be ignored, and an empty message aborts the commit.

--- 04-guess-my-number-game/04-guess-my-number-game/starter/script.js
+++ 04-guess-my-number-game/04-guess-my-number-game/starter/script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+//DOM ELEMENT SELECTION
+
+const messageEl = document.querySelector('.message');
+console.log(messageEl);
+console.log(messageEl.textContent);
+// messageEl.textContent = 'Hello from Javascript';
+
+const scoreEl = document.querySelector('.score');
+console.log(scoreEl);
+console.log(scoreEl.textContent);
+// scoreEl.textContent = 50;
+
+const numberEl = document.querySelector('.number');
+// numberEl.textContent = 15;
+
+const highScoreOfEl = document.querySelector('.highscore');
+// highScoreOfEl.textContent = 30;
+
+const guessEl = document.querySelector('.guess');
+// guessEl.value = 12;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log('Secret Number: ', secretNumber);
+
+let score = 20;
+let highscore = 0;
+
+//change the value of score UI dynamically
+document.querySelector('.score').textContent = score;
+document.querySelector('.highscore').textContent = highscore;
+
+console.log('Game state initialized!');
+
+//Basic Game Logic
+
+document.querySelector('.check').addEventListener('click', function () {
+  //once checked button is click, do this
+  console.log('Check button has been clicked');
+  const guess = Number(document.querySelector('.guess').value);
+  console.log('Players guessed:', guess);
+
+  if (guess === secretNumber) {
+    console.log('Your guess is correct!');
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    console.log('Your guess number is too High!');
+    document.querySelector('.message').textContent = 'Too high!!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
+  }
+});
